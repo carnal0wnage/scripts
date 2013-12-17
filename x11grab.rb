53483bf237a1dd2bdbbc6ec40b9bec309db87281
Joe Barrett <barrettj@gmail.com>
2013-12-16 21:33:15 -0500

    Updated with dates/etc.

--- x11grab.rb
+++ x11grab.rb
@@ -3,9 +3,12 @@
 ip = ARGV[0].to_s.chomp
 display = ARGV[1].to_s.chomp
 
-print "xwd -root -screen -silent -display #{ip}:#{display} > screenshot-#{ip}.xwd" 
-system( "xwd -root -screen -silent -display #{ip}:#{display} > screenshot-#{ip}.xwd" )
-print "convert screenshot-#{ip}.xwd screenshot-#{ip}.png" 
-system( "convert screenshot-#{ip}.xwd screenshot-#{ip}.png" )
-print "dillo screenshot-#{ip}.png"
-system( "dillo screenshot-#{ip}.png")
+d = `date +%F:%H-%M`.chomp
+dir = "~/Foreground/Ops"
+
+print "xwd -root -screen -silent -display #{ip}:#{display} > #{dir}/screenshot-#{ip}-#{d}.xwd" 
+system( "xwd -root -screen -silent -display #{dir}/#{ip}:#{display} > #{dir}/screenshot-#{ip}-#{d}.xwd" )
+print "convert #{dir}/screenshot-#{ip}-#{d}.xwd #{dir}/screenshot-#{ip}-#{d}.png" 
+system( "convert #{dir}/screenshot-#{ip}-#{d}.xwd #{dir}/screenshot-#{ip}-#{d}.png" )
+print "dillo #{dir}/screenshot-#{ip}-#{d}.png"
+system( "dillo #{dir}/screenshot-#{ip}-#{d}.png")
