71b64603304ab80d6fa8dc0fd4331a6c297f3743
Joe Barrett <barrettj@gmail.com>
2015-11-27 14:03:16 -0500

    If we're nuking $PATH, need to qualify paths

--- Scripter.rb
+++ Scripter.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 #
 # The purpose of this is to ensure that our actions while on a pentest are logged properly
 # Each shell window should be scripted out to the right location
@@ -17,7 +17,7 @@ ppid = Process.ppid
 # Print the command to run:
 print "**** Copy and Paste the below for a scripted window ****\n\n"
 
-print "script -a #{opsdir}/script-#{d}_pid#{ppid}.txt\n"
+print "/usr/bin/script -a #{opsdir}/script-#{d}_pid#{ppid}.txt\n"
 print "export PS1=\"%{$fg[red]%}[SCRIPTED]%{$reset_color%} $PS1\"\n"
 # Other environment variables we should build:
 # * ops directory where metasploit/armitage logs end up
